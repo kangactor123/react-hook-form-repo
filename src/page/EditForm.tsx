fbcfe5876b946076bb50794a7bbdc381e139b15c
kangactor123 <kangactor123@naver.com>
2022-12-18 15:04:37 +0900

    Edit form update

--- src/page/EditForm.tsx
+++ src/page/EditForm.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { TForm } from "common/type";
 import MInputText from "component/mui/MInputText";
 import MRadio, { TRadioGroup } from "component/mui/MRadio";
 import MSelect, { ISelectItem } from "component/mui/MSelect";
 import { getList } from "module/api";
-import { useEffect, useLayoutEffect } from "react";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
 import { Button, Label, Row } from "style";
@@ -13,29 +14,50 @@ type EditProps = {
   mode: "edit" | "create";
 };
 
+const defaultValues = {
+  id: "",
+  pwd: "",
+  name: "",
+  phone: "",
+  email: "",
+  occupation: "",
+  identity: "",
+};
+
+const selectList: ISelectItem[] = [
+  { label: "child", value: "child" },
+  { label: "father", value: "father" },
+  { label: "mother", value: "mother" },
+  { label: "grandfather", value: "mather", disabled: true },
+  { label: "grandmother", value: "grandmother" },
+];
+
+const radioGroup: TRadioGroup[] = [
+  { label: "professor", value: "professor" },
+  { label: "student", value: "student" },
+];
+
 function EditForm({ mode }: EditProps) {
   const { id } = useParams();
   const navigate = useNavigate();
-  const handleBack = () => {
-    navigate("/");
-  };
   const {
     control,
     handleSubmit: onSubmit,
     reset,
     watch,
   } = useForm<TForm>({
-    defaultValues: {
-      id: "",
-      pwd: "",
-      name: "",
-      phone: "",
-      email: "",
-      occupation: "",
-      identity: "",
-    },
+    defaultValues,
   });
-  const { data } = useQuery(["list"], getList, {
+
+  const handleBack = () => {
+    navigate("/");
+  };
+
+  const handleSubmit = (data: TForm) => {
+    console.log(data);
+  };
+
+  useQuery(["list"], getList, {
     enabled: !!id,
     refetchOnMount: "always",
     onSuccess: (data: TForm) => {
@@ -43,8 +65,6 @@ function EditForm({ mode }: EditProps) {
     },
     select: (data: TForm[]) => data.find((data) => data.id === id) as TForm,
   });
-
-  console.log(watch());
 
   useEffect(() => {
     if (mode === "create") {
@@ -55,23 +75,6 @@ function EditForm({ mode }: EditProps) {
       });
     }
   }, []);
-
-  const selectList: ISelectItem[] = [
-    { label: "child", value: "child" },
-    { label: "father", value: "father" },
-    { label: "mother", value: "mother" },
-    { label: "grandfather", value: "mather", disabled: true },
-    { label: "grandmother", value: "grandmother" },
-  ];
-
-  const radioGroup: TRadioGroup[] = [
-    { label: "professor", value: "professor" },
-    { label: "student", value: "student" },
-  ];
-
-  const handleSubmit = (data: TForm) => {
-    console.log(data);
-  };
 
   return (
     <form onSubmit={onSubmit(handleSubmit)}>
@@ -95,10 +98,16 @@ function EditForm({ mode }: EditProps) {
         <Label>Phone</Label>
         <MInputText<TForm> control={control} name="phone" />
       </Row>
-      <Row>
-        <Label>E-mail</Label>
-        <MInputText<TForm> control={control} name="email" />
-      </Row>
+      {watch("occupation") === "professor" ? (
+        <Row>
+          <Label>E-mail</Label>
+          <MInputText<TForm>
+            control={control}
+            name="email"
+            rules={{ required: watch("occupation") === "professor" }}
+          />
+        </Row>
+      ) : null}
       <Row>
         <Label>Identity</Label>
         <MSelect<TForm>
@@ -108,9 +117,8 @@ function EditForm({ mode }: EditProps) {
           placeholder={"Choose One"}
         />
       </Row>
-      <Button>Submit</Button>
       <br />
-      <br />
+      <Button>Submit</Button>&nbsp;&nbsp;
       <Button onClick={handleBack}>Back</Button>
     </form>
   );
